9edf92a3d5f965cc9c247f572bcb4378df99f038
RobertLanting <robertlanting02@gmail.com>
2022-09-21 21:46:07 +0200

    update

--- db/db.py
+++ db/db.py
@@ -3,8 +3,18 @@ import sqlite3
 from flask import g
 
 
+def init_db():
+    sqlite3.connect("flaskr.sqlite").execute(
+    "create table challenges (id integer primary key autoincrement,"
+    " name text,"
+    " description text,"
+    " start_date text,"
+    " end_date text,"
+    " done text)")
+
 def get_db():
     if 'db' not in g:
+        init_db()
         g.db = sqlite3.connect("flaskr.sqlite")
         g.db.row_factory = sqlite3.Row
 
